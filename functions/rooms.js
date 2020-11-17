1f0bd2eba4f1f14ef47b561547a03d749e7c66b1
Kimberlee Johnson <kimberlee@Kimberlees-MacBook-Pro.local>
2020-11-17 13:29:14 -0800

    Fixed .prettierrc spelling so singleQuote would apply, thanks to Christian

--- functions/rooms.js
+++ functions/rooms.js
@@ -2,15 +2,15 @@
 // where your node app starts
 // we've started you off with Express (https://expressjs.com/) and axios (https://github.com/axios/axios)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const compression = require("compression");
-const serverless = require("serverless-http");
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const serverless = require('serverless-http');
 
 // MAKE SURE YOU HAVE ADDED YOUR API KEY IN THE .env file or an accessible environment variable
-const BASE_URL = "https://api.daily.co/v1/";
+const BASE_URL = 'https://api.daily.co/v1/';
 const API_AUTH = process.env.DAILY_API_KEY;
 
 const app = express();
@@ -19,18 +19,18 @@ const router = express.Router();
 router.use(compression());
 
 const routerBasePath =
-  process.env.NODE_ENV === "dev" ? `/rooms` : `/.netlify/functions/rooms`;
+  process.env.NODE_ENV === 'dev' ? `/rooms` : `/.netlify/functions/rooms`;
 
 console.log(routerBasePath);
 
 //rooms - https://docs.daily.co/reference#list-rooms
 
-router.post("/", async (request, response) => {
+router.post('/', async (request, response) => {
   try {
-    const rooms = await apiHelper("post", "/rooms", request.body);
+    const rooms = await apiHelper('post', '/rooms', request.body);
     response.json(rooms);
   } catch (e) {
-    console.log("error: ", e);
+    console.log('error: ', e);
     response.status(500).json({ error: e.message });
   }
 });
@@ -53,8 +53,8 @@ const apiHelper = async (method, endpoint, body = {}) => {
     });
     return response.data;
   } catch (error) {
-    console.log("Status: ", error.response.status);
-    console.log("Text: ", error.response.statusText);
+    console.log('Status: ', error.response.status);
+    console.log('Text: ', error.response.statusText);
     // need to throw again so error is caught
     // a possible improvement here is to pass the status code back so it can be returned to the user
     throw new Error(error);
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Helper function prints events to the console
 // Will be passed as callback to callFrame event handlers
 function showEvent(e) {
-  console.log("video call event -->", e);
+  console.log('video call event -->', e);
 }
 
 async function run() {
@@ -9,18 +9,18 @@ async function run() {
   // in the following format:
   // https://some-netlify-url.com/?room=https://mysubdomain.daily.co/roomname&screenshare=true
   const params = new URLSearchParams(window.location.search);
-  const room = params.get("room") || "INSERT_FALLBACK_ROOM_URL";
-  const shareScreenOnJoin = params.get("screenshare");
+  const room = params.get('room') || 'INSERT_FALLBACK_ROOM_URL';
+  const shareScreenOnJoin = params.get('screenshare');
 
   // Create the DailyIframe, passing styling properties to make it fullscreen
   window.callFrame = window.DailyIframe.createFrame({
     iframeStyle: {
-      position: "fixed",
+      position: 'fixed',
       border: 0,
       top: 0,
       left: 0,
-      width: "100%",
-      height: "100%",
+      width: '100%',
+      height: '100%',
     },
   });
 
@@ -34,25 +34,25 @@ async function run() {
 
   // Install event handlers
   callFrame
-    .on("loading", showEvent)
-    .on("loaded", showEvent)
-    .on("started-camera", showEvent)
-    .on("camera-error", showEvent)
-    .on("joining-meeting", showEvent)
-    .on("joined-meeting", doAfterJoin)
-    .on("participant-joined", showEvent)
-    .on("participant-updated", showEvent)
-    .on("participant-left", showEvent)
-    .on("recording-started", showEvent)
-    .on("recording-stopped", showEvent)
-    .on("recording-stats", showEvent)
-    .on("recording-error", showEvent)
-    .on("recording-upload-completed", showEvent)
-    .on("app-message", showEvent)
-    .on("input-event", showEvent)
-    .on("error", showEvent)
+    .on('loading', showEvent)
+    .on('loaded', showEvent)
+    .on('started-camera', showEvent)
+    .on('camera-error', showEvent)
+    .on('joining-meeting', showEvent)
+    .on('joined-meeting', doAfterJoin)
+    .on('participant-joined', showEvent)
+    .on('participant-updated', showEvent)
+    .on('participant-left', showEvent)
+    .on('recording-started', showEvent)
+    .on('recording-stopped', showEvent)
+    .on('recording-stats', showEvent)
+    .on('recording-error', showEvent)
+    .on('recording-upload-completed', showEvent)
+    .on('app-message', showEvent)
+    .on('input-event', showEvent)
+    .on('error', showEvent)
     // Add a leave handler to clean things up
-    .on("left-meeting", leave);
+    .on('left-meeting', leave);
 
   // Join the room
   await callFrame.join({
@@ -64,28 +64,28 @@ async function run() {
   function leave(e) {
     showEvent(e);
     callFrame.destroy();
-    document.getElementsByClassName("start-button")[0].style.display =
-      "initial";
+    document.getElementsByClassName('start-button')[0].style.display =
+      'initial';
   }
 
   // Once a call starts running, hide the start button and prompts
-  document.getElementsByClassName("start-button")[0].style.display = "none";
+  document.getElementsByClassName('start-button')[0].style.display = 'none';
 
   // Log information about the call to the console
   console.log(
-    " You are connected to",
+    ' You are connected to',
     callFrame.properties.url,
-    "\n",
-    "Use the window.callFrame object to experiment with",
-    "\n",
-    "controlling this call. For example, in the console",
-    "\n",
-    "try",
-    "\n",
-    "    callFrame.participants()",
-    "\n",
-    "    callFrame.setLocalVideo(false)",
-    "\n",
-    "    callFrame.startScreenShare()"
+    '\n',
+    'Use the window.callFrame object to experiment with',
+    '\n',
+    'controlling this call. For example, in the console',
+    '\n',
+    'try',
+    '\n',
+    '    callFrame.participants()',
+    '\n',
+    '    callFrame.setLocalVideo(false)',
+    '\n',
+    '    callFrame.startScreenShare()'
   );
 }
